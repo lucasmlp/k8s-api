b2b86c7208419b3676fd063ffb5c323cf56dc0dd
Lucas Machado <lucasml.bh@gmail.com>
2022-08-28 18:02:39 -0300

    Adding list release method on helm adapter

--- adapters/helm/adapter.go
+++ adapters/helm/adapter.go
@@ -1,8 +1,16 @@
 package helm
 
-import "github.com/machado-br/k8s-api/adapters/models"
+import (
+	"log"
+	"os"
+
+	"github.com/machado-br/k8s-api/adapters/models"
+	"helm.sh/helm/v3/pkg/action"
+	"helm.sh/helm/v3/pkg/cli"
+)
 
 type adapter struct{
+	action *action.Configuration
 }
 
 type Adapter interface{
@@ -11,9 +19,33 @@ type Adapter interface{
 
 func NewAdapter(
 ) (adapter, error) {
+	// kubeconfigPath := "/config/kube"
+	// releaseNamespace := "default"
+
+	// actionConfig := new(action.Configuration)
+	// if err := actionConfig.Init(kube.GetConfig(kubeconfigPath, "", releaseNamespace), releaseNamespace, os.Getenv("HELM_DRIVER"), log.Printf); err != nil {
+	// 	panic(err)
+	// }
+
+    settings := cli.New()
+	actionConfig := new(action.Configuration)
+	if err := actionConfig.Init(settings.RESTClientGetter(), settings.Namespace(), os.Getenv("HELM_DRIVER"), log.Printf); err != nil {
+        log.Printf("%+v", err)
+        os.Exit(1)
+    }
+
 	return adapter{
+		action: actionConfig,
 	}, nil
 }
 func (a adapter) ListReleases() []models.Release{
+
+	listAction := action.NewList(a.action)
+	releases, err := listAction.Run();
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	log.Printf("releases: %v\n", releases)
 	return []models.Release{}
 }
